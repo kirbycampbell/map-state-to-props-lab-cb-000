9dd7bb66e4a74d64b23af8793404f8a90e834615
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-16 07:04:30 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App'
-import 
+import
 
 import manageUsers from './reducers/manageUsers'
-
-
-// add imports and code
 
 
 ReactDOM.render(
