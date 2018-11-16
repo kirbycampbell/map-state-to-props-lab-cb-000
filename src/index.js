7c1f13828639cff262d09cb29640da3c2d8aea74
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-16 07:05:33 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import
 
 import manageUsers from './reducers/manageUsers'
 
+let store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
